178b05a9aa0b3df2d32c68deb806858e4c0dbc14
Moeen <moeen12yt@gmail.com>
2023-09-19 22:05:36 +0500

    Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         'custom': '0px 2px 4px 0px rgba(171, 190, 209, 0.20)',
+        'custom2': '0px 8px 16px 0px rgba(171, 190, 209, 0.40)',
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif']
